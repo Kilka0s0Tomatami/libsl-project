8461231c5b3cc8e6ec39bb40af3071098881def8
Kilka0s0Tomatami <karatel25800@gmail.com>
2025-03-25 14:48:46 +0300

    new lib

---
+++ libsl-parser/include/lib.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "dll_modifier.h"
+
+DLL_MODIFIER void lib_print();
---
+++ libsl-parser/lib.cpp
@@ -0,0 +1,8 @@
+#define BUILD_LIB
+#include "lib.h"
+
+#include <iostream>
+
+void lib_print(){
+     std::cout << "Hello World!" << std::endl;
+}
